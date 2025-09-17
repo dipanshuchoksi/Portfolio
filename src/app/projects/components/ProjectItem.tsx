b6e3507dea521b066b8b56b34d6fb1c2be9601a0
Dipanshu Choksi <kbdipanshuchoksi001088@gmail.com>
2025-09-17 22:59:10 +0530

    created ui for projectPage

---
+++ src/app/projects/components/ProjectItem.tsx
@@ -0,0 +1,29 @@
+import Image from "next/image";
+
+function ProjectItem() {
+  return (
+    <div className="rounded-2xl flex gap-10">
+      <div className="w-fit">
+        <Image
+          src="/netflixGpt.png"
+          alt="netflixgpt project image"
+          width={500}
+          height={200}
+          className="rounded-xl"
+        />
+      </div>
+      <div className="flex flex-col gap-3">
+        <h2 className="text-2xl">NetflixGPT</h2>
+        <p>Techstack : ReactJS, TailwindCSS, Firebase, OpenAI API</p>
+        <p>
+          NetflixGPT is an AI-powered recommendation system that suggests movies
+          based on the mood you describe. Simply tell it how you&apos;re
+          feeling, and it curates a list of films that match your emotional
+          vibe, ensuring the perfect watch for any moment.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default ProjectItem;
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,8 +1,12 @@
+import ProjectItem from "./components/ProjectItem";
+
 function ProjectPage() {
   return (
     <div>
       <h2 className="text-2xl font-bold mt-10 mb-5">Projects</h2>
-      <div>Coming soon😊.</div>
+      <div className="flex flex-col gap-5">
+        <ProjectItem />
+      </div>
     </div>
   );
 }
