23bbd1bb2ca366f2ee0005a7d56d9b9fcd97e53b
Dipanshu Choksi <kbdipanshuchoksi001088@gmail.com>
2025-10-08 05:06:11 +0530

    minorBugFix: change order in escapeHTML function to avoid double escaping

--- src/app/utils/escapeHTML.ts
+++ src/app/utils/escapeHTML.ts
@@ -1,8 +1,8 @@
 export function escapeHTML(value: string) {
   return value
+    .replace(/&/g, "&amp;")
     .replace(/</g, "&lt;")
     .replace(/>/g, "&gt;")
-    .replace(/&/g, "&amp;")
     .replace(/"/g, "&quot;")
     .replace(/'/g, "&#039;");
 }
